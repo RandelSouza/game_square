b40f9da59624895e511dd03d3177ccb322b65502
RandelSouza <randelsouza88@gmail.com>
2019-03-12 12:42:15 -0300

    add class Square

--- Jogo_Square/Square.py
+++ Jogo_Square/Square.py
@@ -1,1 +1,9 @@
 # -*- coding:UTF-8 -*-
+
+class Square(object):
+    def __init__(self, position, image, speed=1, cor=None):
+        print position
+        self.image = image
+        self.position = list(position[:])
+        self.speed = speed
+        self.cor = cor
