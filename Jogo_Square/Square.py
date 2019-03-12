828e1a9a773ee9fac81c78a4d7e37c7788161831
RandelSouza <randelsouza88@gmail.com>
2019-03-12 12:52:04 -0300

    add comment method

--- Jogo_Square/Square.py
+++ Jogo_Square/Square.py
@@ -12,5 +12,6 @@ class Square(object):
         rect = draw_centered(self.image, screen, self.position)
         return rect
 
+    # change name method before
     def movimentar(self, velocidade):
         self.position[1] += velocidade
