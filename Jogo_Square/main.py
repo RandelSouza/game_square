7769204d5ec37b2f19b773dedd884cac3a5435fa
RandelSouza <randelsouza88@gmail.com>
2019-03-12 22:46:54 -0300

    remove a=True

--- Jogo_Square/main.py
+++ Jogo_Square/main.py
@@ -4,12 +4,11 @@ import pygame, random
 import funcoes
 import setup
 background, backgroundRect, screen, q, jogador1, jogador2 = funcoes.iniciar()
-a = True
 
 while setup.RUNNING:
     funcoes.exit_game()
 
-    while a:
+    while True:
         tecla_pressionada = pygame.key.get_pressed()
         funcoes.exit_game()
         setup.clock.tick(60)
